7060a9e4bf2d790ecbf8d192035c1e3a47fa33a2
Bright Kanyange <brightkanyange@outlook.com>
2022-03-28 20:05:39 +0300

    Developed a script for adding projects

--- webApp/models/client.py
+++ webApp/models/client.py
@@ -5,7 +5,7 @@ class Client(models.Model):
     logo = models.FileField(blank=True, null=True)
     name = models.CharField(max_length=100)
     website = models.URLField(blank=True, null=True)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
