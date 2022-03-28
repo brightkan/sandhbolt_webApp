48844abd81cc91d8a3fd4461552c5a1d0e767a57
Bright Kanyange <brightkanyange@outlook.com>
2022-03-28 19:26:32 +0300

    Changed projects table

--- webApp/models/project.py
+++ webApp/models/project.py
@@ -3,14 +3,12 @@ from webApp.models import Client
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=40)
-    started = models.DateField()
-    ended = models.DateField(null=True, blank=True)
+    year = models.CharField(max_length=4)
     description = models.TextField()
     location = models.CharField(max_length=20)
     role = models.CharField(max_length=40)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    picture = models.ImageField()
+    picture = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
