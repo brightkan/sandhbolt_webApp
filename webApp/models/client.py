85ec02917fc185ff12c9ea3dee67068f00faae63
Bright Kanyange <brightkanyange@outlook.com>
2022-03-28 19:21:35 +0300

    Added more characters to name

--- webApp/models/client.py
+++ webApp/models/client.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Client(models.Model):
     logo = models.FileField(blank=True, null=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     website = models.URLField(blank=True, null=True)
     phone_number = models.CharField(max_length=20)
 
