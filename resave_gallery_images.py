1fb44eba190d385ca8a3b65215c549abbd0debf7
Bright Kanyange <brightkanyange@outlook.com>
2023-03-13 02:09:11 +0300

    Changed the python script

--- resave_gallery_images.py
+++ resave_gallery_images.py
@@ -1,3 +1,12 @@
+import os
+import django
+
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sandhbolt.settings')
+
+# Initialize Django
+django.setup()
+
 from webApp.models import GalleryImage
 
 # Retrieve all instances of GalleryImage
